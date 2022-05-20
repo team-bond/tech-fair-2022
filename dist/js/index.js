6d492124e00826885651a0b2e6fa0832fa2bdbcd
iamantha <iamantha@bol.com>
2022-05-20 12:20:15 +0200

    Add images to the model

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,26 +9,32 @@ $("button").click(function () {
     let source = $(this).val();
     let name =  $('#userName').val()
     let modalSource = 'API';
+    $("#ftpImages").hide();
+    $("#apiImages").show();
 
     if (source == "ftp"){
         modalSource = 'FTP Server'
+        $("#ftpImages").show();
+        $("#apiImages").hide();
     }
 
     $("#modalUsername").html(name);
     $("#modalSource").html(modalSource);
 
-    $.ajax({
-        type: "POST",
-        url: "https://fair-bond.herokuapp.com/api/flow/session",
-        data: JSON.stringify({name:name, source: source.toUpperCase()}),
-        contentType: "application/json",
-        encode: true,
-    }).done(function (data) {
-        $("#ftpModal").modal('show');
-        console.log(data);
-        // set the item in localStorage
-        localStorage.setItem('session', JSON.stringify(data));
-    });
+    $("#ftpModal").modal('show');
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: "https://fair-bond.herokuapp.com/api/flow/session",
+    //     data: JSON.stringify({name:name, source: source.toUpperCase()}),
+    //     contentType: "application/json",
+    //     encode: true,
+    // }).done(function (data) {
+    //     $("#ftpModal").modal('show');
+    //     console.log(data);
+    //     // set the item in localStorage
+    //     localStorage.setItem('session', JSON.stringify(data));
+    // });
 });
 
 
